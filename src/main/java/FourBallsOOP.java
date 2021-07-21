8f7493c18930929500d4929540398ba3481d3817
Randheerrrk <randheerrrk@gmail.com>
2021-07-21 15:12:33 +0530

    [Randheer] add. Four Balls OOP implementation

---
+++ src/main/java/FourBallsOOP.java
@@ -0,0 +1,58 @@
+import processing.core.PApplet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FourBallsOOP extends PApplet {
+
+    public static final int HEIGHT = 480;
+    public static final int WIDTH = 640;
+    public static final int OFFSET = HEIGHT / 5;
+    private final List<Ball> ballsList = new ArrayList<>();
+    public static void main(String[] args) {
+        PApplet.main("FourBallsOOP");
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    private class Ball {
+        private int x;
+        private int y;
+        private final int dx;
+        private final int dy;
+        private final int radius;
+        public Ball(int xPos, int yPos, int dx, int dy, int radius, PApplet sketch) {
+            this.x = xPos;
+            this.y = yPos;
+            this.dx = dx;
+            this.dy = dy;
+            this.radius = radius;
+        }
+        private void draw() {
+            ellipse(x, y, radius, radius);
+        }
+        private void update() {
+            x+=dx;
+            y+=dy;
+        }
+    }
+
+    @Override
+    public void setup() {
+        for(int i=1; i<=4; i++) {
+            ballsList.add(new Ball(0, i*OFFSET, i, 0, 10, this));
+        }
+    }
+
+    @Override
+    public void draw() {
+        for(Ball ball: ballsList) {
+            ball.draw();
+            ball.update();
+        }
+    }
+}
