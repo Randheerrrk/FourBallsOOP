d0b50d832f9717a5e61b302e4b948febdb493480
Randheer Ramesh K <randheerrrk@gmail.com>
2021-07-30 11:10:44 +0530

    Update FourBallsOOP.java

--- src/main/java/FourBallsOOP.java
+++ src/main/java/FourBallsOOP.java
@@ -7,8 +7,8 @@ public class FourBallsOOP extends PApplet {
 
     public static final int HEIGHT = 480;
     public static final int WIDTH = 640;
-    public static final int OFFSET = HEIGHT / 5;
-    private List<Ball> ballsList = new ArrayList<>();
+    private final int OFFSET = HEIGHT / 5;
+    private final List<Ball> ballsList = new ArrayList<>();
     
     public static void main(String[] args) {
         PApplet.main("FourBallsOOP");
@@ -19,16 +19,31 @@ public class FourBallsOOP extends PApplet {
         super.settings();
         size(WIDTH, HEIGHT);
     }
+    
+    @Override
+    public void setup() {
+        for(int i=1; i<=4; i++) {
+            ballsList.add(new Ball(0, i*OFFSET, i, 0, 10));
+        }
+    }
+
+    @Override
+    public void draw() {
+        for(Ball ball: ballsList) {
+            ball.draw();
+            ball.update();
+        }
+    }
 
     private class Ball {
         
         private int x;
         private int y;
-        private int dx;
-        private int dy;
-        private int diameter;
+        private int final dx;
+        private int final dy;
+        private int final diameter;
         
-        public Ball(int xPos, int yPos, int dx, int dy, int diameter, PApplet sketch) {
+        public Ball(int xPos, int yPos, int dx, int dy, int diameter) {
             this.x = xPos;
             this.y = yPos;
             this.dx = dx;
@@ -46,18 +61,4 @@ public class FourBallsOOP extends PApplet {
         }
     }
 
-    @Override
-    public void setup() {
-        for(int i=1; i<=4; i++) {
-            ballsList.add(new Ball(0, i*OFFSET, i, 0, 10, this));
-        }
-    }
-
-    @Override
-    public void draw() {
-        for(Ball ball: ballsList) {
-            ball.draw();
-            ball.update();
-        }
-    }
 }
